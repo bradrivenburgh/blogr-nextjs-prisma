ae3b7d7d219976ae4a7892e88697a461d4184ecd
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2023-03-19 21:04:57 -0400

    Implement auth and routes to add a new post

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,52 +1,100 @@
+// Header.tsx
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   const router = useRouter();
   const isActive: (pathname: string) => boolean = (pathname) =>
     router.pathname === pathname;
 
+  const { data: session, status } = useSession();
+
   let left = (
-    <div className="left">
-      <Link className="bold" href="/" data-active={isActive("/")}>
+    <div className={styles.left}>
+      <Link
+        href="/"
+        className={[styles.bold, styles.link].join(" ")}
+        data-active={isActive("/")}
+      >
         Feed
       </Link>
-      <style jsx>{`
-        .bold {
-          font-weight: bold;
-        }
-
-        a {
-          text-decoration: none;
-          color: #000;
-          display: inline-block;
-        }
-
-        .left a[data-active="true"] {
-          color: gray;
-        }
-
-        a + a {
-          margin-left: 1rem;
-        }
-      `}</style>
     </div>
   );
 
   let right = null;
 
+  if (status === "loading") {
+    left = (
+      <div className={styles.left}>
+        <Link
+          href="/"
+          className={[styles.bold, styles.link].join(" ")}
+          data-active={isActive("/")}
+        >
+          Feed
+        </Link>
+      </div>
+    );
+    right = (
+      <div className={styles.right}>
+        <p>Validating session ...</p>
+      </div>
+    );
+  }
+
+  if (!session) {
+    right = (
+      <div className={styles.right}>
+        <Link
+          href="/api/auth/signin"
+          data-active={isActive("/signup")}
+          className={styles.link}
+        >
+          Log in
+        </Link>
+      </div>
+    );
+  }
+
+  if (session) {
+    left = (
+      <div className={styles.left}>
+        <Link
+          href="/"
+          className={[styles.bold, styles.link].join(" ")}
+          data-active={isActive("/")}
+        >
+          Feed
+        </Link>
+        <Link
+          href="/drafts"
+          data-active={isActive("/drafts")}
+          className={[styles.bold, styles.link].join(" ")}
+        >
+          My drafts
+        </Link>
+      </div>
+    );
+    right = (
+      <div className={styles.right}>
+        <p>
+          {session.user.name} ({session.user.email})
+        </p>
+        <Link href="/create" className={styles.link}>
+          <button>New post</button>
+        </Link>
+        <button onClick={() => signOut()}>Log out</button>
+      </div>
+    );
+  }
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       {left}
       {right}
-      <style jsx>{`
-        nav {
-          display: flex;
-          padding: 2rem;
-          align-items: center;
-        }
-      `}</style>
     </nav>
   );
 };
